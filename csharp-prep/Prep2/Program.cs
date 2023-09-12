430d083c544d5f040f1373e2e75644a9f801a1f8
ValRaz <valerie.dres@gmail.com>
2023-09-12 17:54:05 -0600

    letter grade program plus stretch

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,81 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        //Gathers grade percentage from user
+        Console.Write("What is your grade percentage? ");
+        string gradeInput = Console.ReadLine();
+
+        //Converts user input to int
+        int percentage = int.Parse(gradeInput);
+
+        //defines letter grade variable
+        string letterGrade = "";
+
+        //Determines letter grade of user input
+        if (percentage >= 90)
+        {
+            letterGrade = "A";
+        }
+
+        else if (percentage >= 80)
+        {
+            letterGrade = "B";
+        }
+
+        else if (percentage >= 70)
+        {
+            letterGrade = "C";
+        }
+
+        else if (percentage >= 60)
+        {
+            letterGrade = "D";
+        }
+
+        else
+        {
+            letterGrade = "F";
+        }
+
+        //Determines + or - of letter grade
+        string gradeAdditional = "";
+
+        if (percentage % 10 >= 7)
+        {
+            gradeAdditional = letterGrade + "+";
+        }
+
+        else
+        {
+            gradeAdditional = letterGrade + "-";
+        }
+
+        //Recognizes A + and F+/- and adjusts the output accordingly
+        if (gradeAdditional == "A+")
+        {
+            gradeAdditional = "A";
+        }
+
+        else if (gradeAdditional == "F+")
+        {
+            gradeAdditional = "F";
+        }
+
+        else if (gradeAdditional == "F-")
+        {
+            gradeAdditional = "F";
+        }
+
+        //Determines pass/fail per percentage and prints apropriate response
+        if (percentage >=70)
+        {
+            Console.WriteLine($"Your grade is {gradeAdditional}, passing this course. Great work!");
+        }
+
+        else
+        {
+            Console.WriteLine($"Your grade is {gradeAdditional}. Better luck next semester!");
+        }
+
     }
 }
