67dacb596fbabcb5cba2df6f9f26c453f8b1c93e
ValRaz <valerie.dres@gmail.com>
2023-09-12 16:33:24 -0600

    modified to gather, display input-Bond style name

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name?");
+        string firstname = Console.ReadLine();
+        //Retrieves user input storing data in the firstname variable
+
+        Console.Write("What is your last name?");
+        string lastname = Console.ReadLine();
+        //Retrieves user input storing data in the lastname variable
+
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");
+        //Prints Bond style naming to the terminal
+
     }
 }
