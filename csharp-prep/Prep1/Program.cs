dfd92e26e0306021878e907af96c04deb4686b56
ValRaz <valerie.dres@gmail.com>
2023-09-12 16:41:29 -0600

    spacing corrected

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,15 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is your first name?" );
+        Console.Write("What is your first name? ");
         string firstname = Console.ReadLine();
         //Retrieves user input storing data in the firstname variable
 
-        Console.Write("What is your last name?" );
+        Console.Write("What is your last name? ");
         string lastname = Console.ReadLine();
         //Retrieves user input storing data in the lastname variable
 
-        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}." );
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}. ");
         //Prints Bond style naming to the terminal
 
     }
