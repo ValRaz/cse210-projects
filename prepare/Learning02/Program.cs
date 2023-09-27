f97da935ed2fedcee899fa0378e030664ef4ac13
ValRaz <valerie.dres@gmail.com>
2023-09-27 16:50:24 -0600

    Week 3 Learning Activity Completed

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.IO.Pipes;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Squire PC";
+        job1._startYear = 2000;
+        job1._endYear = 2017;
+        //Creates job1 instance of the Job class
+
+        Job job2 = new Job();
+        job2._jobTitle = "Senior Software Engineer";
+        job2._company = "Apple";
+        job2._startYear = 2017;
+        job2._endYear = 2023;
+        //Creates job2 instance of the Job class
+
+        Resume resume1 = new Resume();
+        resume1._name = "Jayde Barr";
+
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.ShowJobInfo();
     }
 }
