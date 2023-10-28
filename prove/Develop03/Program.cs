02e22e1872157edcc9ea75161099d87b83717446
ValRaz <valerie.dres@gmail.com>
2023-10-28 10:49:44 -0600

    filepath error resolution attempt

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -7,10 +7,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        string filePath = "Scriptures.txt";
+        string filePath = "C:/Documents/cse210-projects/prove/Develop03/Scriptures.txt";
         var loadedScripture = new Scripture(filePath);
 
         // Utilizes Word, Reference, and Scripture class to display a scripture, and interact with the user to hide a random word or quit per user input.
+        //Demonstrated creativity by including functionality in the classes to account for the hidden status of words so that a word cannot be selected to be 
+        //hidden when it is already hidden. Demonstrated creativity by housing a number of scriptures in a txt file, then having the code randomly select a 
+        //scripture from the file, load, parse, and display it accordingly. 
         do
         {
             // Creativity demonstration naming and welcome message
--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -17,14 +17,18 @@ public class Scripture {
         _words = words;
     }
 
-    // Initializes a random scripture from a file.
+    // Creativity demonstration: Initializes a random scripture from a file.
     public Scripture(string filePath) {
         var scriptures = LoadScripturesFromFile(filePath);
+        var randomScripture = SelectRandomScripture(scriptures);
+        _scriptureReference = randomScripture._scriptureReference;
+        _words = randomScripture._words;
+    }
+        //Randomly slects a scripture from the list of loaded scriptures
+        private Scripture SelectRandomScripture(List<Scripture> scriptures) {
         var random = new Random();
         var randomIndex = random.Next(scriptures.Count);
-        var selectedScripture = scriptures[randomIndex];
-        _scriptureReference = selectedScripture._scriptureReference;
-        _words = selectedScripture._words;
+        return scriptures[randomIndex];
     }
 
     // Displays the scripture and reference.
@@ -56,7 +60,7 @@ public class Scripture {
         return _words.TrueForAll(word => word.IsHidden);
     }
 
-    // Parses scripture text and reference from a file.
+    //Creativity demonstration: Parses scripture text and reference from a file.
     private List<Scripture> LoadScripturesFromFile(string filePath) {
         var lines = File.ReadAllLines(filePath);
         var result = new List<Scripture>();
@@ -89,6 +93,7 @@ public class Scripture {
                     }
                 }
             }
+            Console.WriteLine("Loaded Scriptures Count: " + result.Count);
         }
         return result;
     }
