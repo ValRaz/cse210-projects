a8618f2bdd3248b5c7176d7a1cc3a1b893fa354a
ValRaz <valerie.dres@gmail.com>
2023-11-08 16:53:53 -0700

    learning activity completed final push

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -6,19 +6,28 @@ class Program
     static void Main(string[] args)
     {
         //Create a list to hold shapes
-        List<Shape> shapesList = new List<Shape>();
+        List<Shape> shapes = new List<Shape>();
 
-        //Add a square, rectangle, and circle to shapesList
-        shapesList.Add(new Square("Teal", 7.0));
-        shapesList.Add(new Rectangle("Purple", 9.0, 5.0));
-        shapesList.Add(new Circle("Green", 15.5));
+        //Creates Square object
+        Square listShapes1 = new Square("Teal", 3.5);
+        shapes.Add(listShapes1);
 
-        //Iterate through shapesList
-        foreach (Shape shape in shapesList) {
-            
-            //Display the color and area of each shape
-            Console.WriteLine($"Color: {shape.GetColor()}");
-            Console.WriteLine($"Area: {shape.GetArea()}");
+        //Creates Rectangle object
+        Rectangle listShapes2 = new Rectangle("Blue", 9.0, 5.0);
+        shapes.Add(listShapes2);
+
+        //Creates Circle object
+        Circle listShapes3 = new Circle("Purple", 15.5);
+        shapes.Add(listShapes3);
+
+        //Iterates through shapes list and displays color and area
+        foreach (Shape s in shapes) {
+            //Retrieves color from base class
+            string color = s.GetColor();
+            //Calculates area per shape from derived class methods
+            double area = s.GetArea();
+            //Displays the data for each shape
+            Console.WriteLine($"The {color} shape has an area of {area}.");
         }
-    }    
+    }
 }
