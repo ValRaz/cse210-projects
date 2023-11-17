0cddf3b19f4148830d454de983b89b8df99ec3bc
ValRaz <valerie.dres@gmail.com>
2023-11-16 19:44:02 -0700

    working program debugging push five

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,21 +4,20 @@ using System.IO;
 
 class Program
 {
-    // Entry point of the program, handles goal loading, user actions, and goal saving.
     static void Main(string[] args) {
         List<Goal> goals = LoadGoals();
         ProcessUserActions(goals);
         SaveGoals(goals);
     }
 
-    // Loads goals from a file or initializes an empty list if no file exists.
-    static List<Goal> LoadGoals(){
+    //Loads goals from a file or initializes an empty list if no file exists.
+    static List<Goal> LoadGoals() {
         List<Goal> loadedGoals = new List<Goal>();
-        // Checks for the existence of the file and loads goal data if available.
-        if (File.Exists("Goals.txt")){
-            using (StreamReader reader = new StreamReader("Goals.txt")){
+        //Checks for the existence of the file and loads goal data if available.
+        if (File.Exists("Goals.txt")) {
+            using (StreamReader reader = new StreamReader("Goals.txt")) {
                 string line;
-                // Parses each line from the file to create Goals.
+                //Parses each line from file to create Goals.
                 while ((line = reader.ReadLine()) != null){
                     string[] goalData = line.Split(',');
                     string name = goalData[0].Trim();
@@ -26,7 +25,7 @@ class Program
                     int points = int.Parse(goalData[2].Trim());
 
                     Goal goal;
-                    // Creates goal instances based on completion status.
+                    //Creates goals based on completion status.
                     if (completionStatus == "[ ]"){
                         goal = new SimpleGoal(name, completionStatus, points);
                     }
@@ -41,23 +40,21 @@ class Program
                 }
             }
         }
-        else
-        {
+        else {
             Console.WriteLine("No existing goals found. Initializing an empty list.");
         }
 
         return loadedGoals;
     }
 
-    // Manages user interactions and processes their chosen actions.
-    static void ProcessUserActions(List<Goal> goals){
+    //Manages user interactions and processes their chosen actions.
+    static void ProcessUserActions(List<Goal> goals) {
         Console.WriteLine("1. Create Goal\n2. Record Event\n3. Show Goals\n4. Display User's Score\n5. Exit");
         bool exit = false;
-        // Handles user input for goal actions.
-        while (!exit)
-        {
+        //Handles user input for goal actions.
+        while (!exit) {
             Console.Write("Enter your choice: ");
-            if (int.TryParse(Console.ReadLine(), out int choice)){
+            if (int.TryParse(Console.ReadLine(), out int choice)) {
                 switch (choice){
                     case 1:
                         CreateGoal(goals);
@@ -79,16 +76,16 @@ class Program
                         break;
                 }
             }
-            else{
+            else {
                 Console.WriteLine("Invalid input. Please enter a number.");
             }
         }
     }
 
-    // Collects user input to create a new goal and adds it to the list.
-    static void CreateGoal(List<Goal> goals){
+    //Collects user input to create a new goal and adds it to the list.
+    static void CreateGoal(List<Goal> goals) {
         Console.WriteLine("Creating a Goal:");
-        // Collects information for a new goal.
+        //Collects information for a new goal.
         Console.Write("Enter Goal Name: ");
         string name = Console.ReadLine();
         Console.Write("Enter Goal Description: ");
@@ -99,7 +96,7 @@ class Program
             Console.WriteLine("Invalid input. Please enter a valid number.");
             Console.Write("Enter Amount of Points: ");
         }
-        // Provides options for goal selection.
+        //Provides options for goal selection.
         Console.WriteLine("Select Goal Type:");
         Console.WriteLine("1. Simple Goal");
         Console.WriteLine("2. Eternal Goal");
@@ -111,8 +108,8 @@ class Program
         }
 
         Goal goal;
-        // Creates different goal types based on user input.
-        switch (type){
+        //Creates different goal types based on user input.
+        switch (type) {
             case 1:
                 goal = new SimpleGoal(name, description, amountPoints);
                 break;
@@ -122,8 +119,7 @@ class Program
             case 3:
                 Console.Write("Enter Times to Accomplish Goal: ");
                 int timesToAccomplish;
-                while (!int.TryParse(Console.ReadLine(), out timesToAccomplish))
-                {
+                while (!int.TryParse(Console.ReadLine(), out timesToAccomplish)) {
                     Console.WriteLine("Invalid input. Please enter a valid number.");
                     Console.Write("Enter Times to Accomplish Goal: ");
                 }
@@ -137,80 +133,76 @@ class Program
         Console.WriteLine("Goal created successfully!");
     }
 
-    // Records an event for a selected goal and updates status.
-    static void RecordEvent(List<Goal> goals){
+    //Records event for a goal and updates status.
+    static void RecordEvent(List<Goal> goals) {
         Console.WriteLine("Recording an Event:");
         ShowGoals(goals);
         Console.Write("Select a Goal to Record Event (Enter Goal Number): ");
-        if (int.TryParse(Console.ReadLine(), out int goalNumber) && goalNumber >= 0 && goalNumber < goals.Count)
-        {
+        if (int.TryParse(Console.ReadLine(), out int goalNumber) && goalNumber >= 0 && goalNumber < goals.Count) {
             goals[goalNumber].RecordEvent();
         }
-        else
-        {
+        else {
             Console.WriteLine("Invalid goal number.");
         }
     }
 
-    // Displays the list of goals with completion status and points.
-    static void ShowGoals(List<Goal> goals){
+    //Displays the list of goals with completion status and points.
+    static void ShowGoals(List<Goal> goals) {
         Console.WriteLine("List of Goals:");
-        for (int i = 0; i < goals.Count; i++)
-        {
+        for (int i = 0; i < goals.Count; i++) {
             Console.WriteLine($"{i}. {goals[i].GetName()} - Completion: {goals[i].GetCompletion()} - Points: {goals[i].GetPoints()}");
         }
     }
 
-    // Calculates and displays the total score of all completed goals.
-    static void DisplayUserScore(List<Goal> goals){
+    //Calculates and displays the total score of all completed goals.
+    static void DisplayUserScore(List<Goal> goals) {
         int totalScore = 0;
-        foreach (Goal goal in goals)
-        {
+        foreach (Goal goal in goals) {
             totalScore += goal.GetPoints();
         }
         Console.WriteLine($"Total User Score: {totalScore}");
     }
 
-    // Updates the file with most recently updated goal information.
-    static void SaveGoals(List<Goal> goals){
+    //Updates the file with most recently updated goal information.
+    static void SaveGoals(List<Goal> goals) {
         List<string> updatedLines = new List<string>();
-        // Checks for the existing file and updates goal information.
-        if (File.Exists("prove/Develop05/Goals.txt")){
+        //Checks for the existing file and updates goal information.
+        if (File.Exists("prove/Develop05/Goals.txt")) {
             string[] lines = File.ReadAllLines("prove/Develop05/Goals.txt");
             
-            foreach (var line in lines){
+            foreach (var line in lines) {
                 string[] goalData = line.Split(',');
                 string name = goalData[0].Trim();
                 string completionStatus = goalData[1].Trim();
                 int points = int.Parse(goalData[2].Trim());
 
                 Goal existingGoal = goals.Find(g => g.GetName() == name);
-                if (existingGoal != null){
-                    if (existingGoal.GetCompletion() == "[X]" && completionStatus == "[ ]"){
-                        // Updates completion status to 'Completed' in the file.
+                if (existingGoal != null) {
+                    if (existingGoal.GetCompletion() == "[X]" && completionStatus == "[ ]") {
+                        //Updates completion status to 'Completed' in the file.
                         updatedLines.Add($"{name}, Completed, {points}");
                     }
-                    else if (existingGoal is CheckListGoal checklistGoal && completionStatus != "[ ]"){
-                        // Updates times completed for the ChecklistGoal in the file.
+                    else if (existingGoal is CheckListGoal checklistGoal && completionStatus != "[ ]") {
+                        //Updates times completed for the ChecklistGoal in the file.
                         string[] completionParts = completionStatus.Split('/');
                         int timesCompleted = int.Parse(completionParts[0].Substring(completionParts[0].LastIndexOf(' ') + 1));
                         string updatedCompletionStatus = $"Completed {checklistGoal.GetTimesAccomplished()}/{timesCompleted} times";
                         updatedLines.Add($"{name}, {updatedCompletionStatus}, {points}");
                     }
-                    else{
+                    else {
                         updatedLines.Add($"{name}, {completionStatus}, {points}");
                     }
                 }
-                else{
+                else {
                     updatedLines.Add($"{name}, {completionStatus}, {points}");
                 }
             }
         }
-        else{
+        else {
             Console.WriteLine("No existing goals found. Initializing an empty list.");
         }
 
-        // Writes the updated goal information to the file.
+        //Writes the updated goal information to the file.
         File.WriteAllLines("prove/Develop05/Goals.txt", updatedLines);
         Console.WriteLine("Goals saved to file.");
     }
